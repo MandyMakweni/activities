8a4f2d823451d7519a76a59f2ce361b074541328
mandymshy <mandymakweni1@gmail.com>
2018-01-09 13:27:39 +0200

    changes

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function scrollFunction() {
     }
 }
 
+<<<<<<< HEAD
+// sort
+
+=======
+>>>>>>> 8b162113c32ea83c2f658137b6eca62a21b2dcd7
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
@@ -21,3 +26,74 @@ function topFunction() {
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-46156385-1', 'cssscript.com');
   ga('send', 'pageview');
+<<<<<<< HEAD
+
+function sortList() {
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("id01");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    //Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*check if the next item should
+      switch place with the current item:*/
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        /*if next item is alphabetically
+        lower than current item, mark as a switch
+        and break the loop:*/
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark the switch as done:*/
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
+
+// json data
+$("button").click ( function() {
+
+$.getJSON("json data.json", function(obj){
+$.each(obj, function(key, value){
+$("ul").append("<li>"+value.name+"</li>");
+
+
+});
+
+});
+
+});
+
+//slide
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+=======
+>>>>>>> 8b162113c32ea83c2f658137b6eca62a21b2dcd7
